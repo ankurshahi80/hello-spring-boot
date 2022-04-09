d317ac68a100e138178de44a868925d32cefa745
Ankur Shahi <shahiankur80@gmail.com>
2022-04-09 12:15:49 -0400

    started server and defined a test endpoint using Spring MVC annotations

---
+++ src/main/java/com/revature/HelloSpringBoot/repositories/CandyRepository.java
@@ -0,0 +1,11 @@
+//package com.revature.HelloSpringBoot.repositories;
+//
+//import com.revature.HelloSpringBoot.model.Candy;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//@EnableJpaRepositories
+//public interface CandyRepository extends JpaRepository<Candy, Integer> {
+//}
---
+++ src/main/java/com/revature/HelloSpringBoot/services/CandyService.java
@@ -0,0 +1,16 @@
+//package com.revature.HelloSpringBoot.services;
+//
+//import com.revature.HelloSpringBoot.model.Candy;
+//
+//import java.util.List;
+//
+//public interface CandyService {
+//    // Basic crud business logic
+//    public Candy createCandy(Candy c);
+//
+//    public List<Candy> getAll();
+//
+//    public Candy editCandy(Candy c);
+//
+//    public void deleteCandyById(int candyId);
+//}
