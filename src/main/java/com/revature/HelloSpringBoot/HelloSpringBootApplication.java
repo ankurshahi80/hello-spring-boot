9b70755da432d1a07ad968cf917f76a4c379d881
Ankur Shahi <shahiankur80@gmail.com>
2022-04-09 21:27:35 -0400

    Added loggingAspect using Spring Boot's AOP (Aspect Oreiented Programming)

--- src/main/java/com/revature/HelloSpringBoot/HelloSpringBootApplication.java
+++ src/main/java/com/revature/HelloSpringBoot/HelloSpringBootApplication.java
@@ -2,12 +2,14 @@ package com.revature.HelloSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableJpaRepositories
+@EnableAspectJAutoProxy
 public class HelloSpringBootApplication {
 
 	public static void main(String[] args) {
